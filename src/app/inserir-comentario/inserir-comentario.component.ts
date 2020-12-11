db97cbbb9fdc127d390ab8d37dc353f4d6c5091c
rafael.dyszy <rafaeldyszy@hotmail.com>
2020-12-11 10:51:24 -0500

    fix layout entrada manual

---
+++ src/app/inserir-comentario/inserir-comentario.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-inserir-comentario',
+  templateUrl: './inserir-comentario.component.html',
+  styleUrls: ['./inserir-comentario.component.scss'],
+})
+export class InserirComentarioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
